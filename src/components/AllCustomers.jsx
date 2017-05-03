c5ddfafa44575bb9a66abdfaa2a1ac8013be914b
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-03 16:16:24 +0500

    Imprivment

--- src/components/AllCustomers.jsx
+++ src/components/AllCustomers.jsx
@@ -23,156 +23,43 @@ export class AllCustomersComponent extends Component {
 
 
     render() {
-        // const center = {
-        //     width: '100%',
-        //     margin: '0 auto',
-        //     background: 'darkCyan',
-        //     color : 'white !important',
-        //     padding: '30px',
-        //     boxShadow: '0 15px 20px rgba(0, 0, 0, 0.3)'
-        // }
         const center = {
-            width: '80%',
+            width: '70%',
             margin: '0 auto',
             background: 'darkCyan',
-            color: 'white !important',
-            padding: '30px',
-            boxShadow: '0 15px 20px rgba(0, 0, 0, 0.3)'
         }
-        const centeres = {
+        const container = {
             width: '100%',
             margin: '0 auto',
-            background: '#ffe6cc',
+            background: '#fff2e6',
             color: 'white !important',
-            padding: '30px',
             boxShadow: '0 15px 20px rgba(0, 0, 0, 0.3)'
         }
-        console.log(this.state)
         return (
-            <div style={centeres}>
-                {/*{this.state.customerArray.map((value, index) => {
-                    console.log('index', index)
-                    console.log('value', value)
-                    return (
-                        <h2 key={index}>
-                            {value.customerName}
-                        </h2>
-                    )
-                })}*/}
-
+            <div style={container}>
                 <br />
                 <br />
                 <mat.Table style={center}>
-                    <mat.TableHeader displaySelectAll={false}>
-                        <mat.TableRow style={{ color: 'white' }}>
-                            <mat.TableHeaderColumn>Name</mat.TableHeaderColumn>
-                            <mat.TableHeaderColumn>Number</mat.TableHeaderColumn>
-                            <mat.TableHeaderColumn>Address</mat.TableHeaderColumn>
-                        </mat.TableRow>
-                    </mat.TableHeader>
                     <mat.TableBody displayRowCheckbox={false}>
-                        <mat.TableRow style={{ color: 'white' }}>
-                            <mat.TableRowColumn>1</mat.TableRowColumn>
-                            <mat.TableRowColumn>John Smith</mat.TableRowColumn>
-                            <mat.TableRowColumn>Employed</mat.TableRowColumn>
+                        <mat.TableRow style={{ color: 'white', fontWeight: '600' }}>
+                            <mat.TableRowColumn>Name</mat.TableRowColumn>
+                            <mat.TableRowColumn>Number</mat.TableRowColumn>
+                            <mat.TableRowColumn>Address</mat.TableRowColumn>
                         </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>2</mat.TableRowColumn>
-                            <mat.TableRowColumn>Randal White</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>3</mat.TableRowColumn>
-                            <mat.TableRowColumn>Stephanie Sanders</mat.TableRowColumn>
-                            <mat.TableRowColumn>Employed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>4</mat.TableRowColumn>
-                            <mat.TableRowColumn>Steve Brown</mat.TableRowColumn>
-                            <mat.TableRowColumn>Employed</mat.TableRowColumn>
-                        </mat.TableRow>
-                        <mat.TableRow>
-                            <mat.TableRowColumn>5</mat.TableRowColumn>
-                            <mat.TableRowColumn>Christopher Nolan</mat.TableRowColumn>
-                            <mat.TableRowColumn>Unemployed</mat.TableRowColumn>
-                        </mat.TableRow>
+
+                        {this.state.customerArray.map((value, index) => {
+                            return (
+                                <mat.TableRow style={{ color: 'white' }} key={index}>
+                                    <mat.TableRowColumn>{value.customerName}</mat.TableRowColumn>
+                                    <mat.TableRowColumn>{value.customerNumber}</mat.TableRowColumn>
+                                    <mat.TableRowColumn>{value.customerAddress}</mat.TableRowColumn>
+                                </mat.TableRow>
+                            )
+                        })}
                     </mat.TableBody>
                 </mat.Table>
+                <br />
+                <br />
             </div>
         )
     }
