a1a6aa1d9f0cd4248e4c9866e226d1c3b7264399
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-09 11:28:43 +0500

    Create and Implement Product Reducer.js

---
+++ src/store/reducers/productReducer.js
@@ -0,0 +1,52 @@
+const initialState = {
+    isLoading: false,
+    isResult: false,
+    // products: {},
+    // productsDetail: {},
+    // storesDetail: {},
+    // stock: { headers: {}, value: {} },
+    PRODUCT: {},
+    isError: false,
+    errorMSG: {},
+    // data: [],
+    // twoKeyList: []
+}
+export const productReducer = (state = initialState, action) => {
+    // console.log('action', action)
+
+    let newState = state
+    switch (action.type) {
+        // Action handlers
+        case 'SET_RESULT_FALSE':
+            return Object.assign({}, state, { isResult: false })
+
+        case 'SET_PRODUCT':
+            return Object.assign({}, state, { isLoading: true })
+
+        case 'SET_PRODUCT_SUCCESS':
+            newState['isLoading'] = false;
+            newState['isResult'] = true
+            return Object.assign({}, state, newState)
+
+        case 'SET_PRODUCT_FAIL':
+            newState['isError'] = true;
+            newState['isLoading'] = false
+            newState['errorMSG'] = action.payload
+            return Object.assign({}, state, newState)
+
+        // Get PRODUCTs actions
+        // case 'GET_PRODUCT':
+        //     return Object.assign({}, state, { isLoading: true })
+
+        // case 'GET_PRODUCT_SUCCESS':
+        //     newState['PRODUCT'] = Object.assign({}, action.payload);
+        //     // newState['isResult'] = true
+        //     return Object.assign({}, state, newState)
+
+        // case 'GET_PRODUCT_FAIL':
+        //     return Object.assign({}, state, { isLoading: false })
+
+        default:
+            return state;
+    }
+}
