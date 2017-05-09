29928d04625f654c99bf1c25646061d5010c277b
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-09 11:30:50 +0500

    Add ProductEpic/Reducer main index file

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { combineEpics, createEpicMiddleware } from 'redux-observable'
 
 // all reducer import
-import { authReducer, branchReducer, departReducer, customerReducer, supplierReducer } from './reducers'
+import { authReducer, branchReducer, departReducer, customerReducer, supplierReducer ,productReducer } from './reducers'
 
 // all Epic import
-import { AuthEpic, BranchesEpic, DepartmentEpic, CustomerEpic, SuppleirEpic } from './epics'
+import { AuthEpic, BranchesEpic, DepartmentEpic, CustomerEpic, SuppleirEpic,ProductEpic } from './epics'
 
 const rootReducer = combineReducers({
     authReducer,
     branchReducer,
     departReducer,
     customerReducer,
-    supplierReducer
+    supplierReducer,
+    productReducer
 })
 
 const allEpics = combineEpics(
@@ -24,7 +25,8 @@ const allEpics = combineEpics(
     CustomerEpic.createCustomer,
     SuppleirEpic.createSupleir,
     CustomerEpic.getCustomer,
-    SuppleirEpic.getSuppleir
+    SuppleirEpic.getSuppleir,
+    ProductEpic.createProduct
 )
 
 const epicMiddleware = createEpicMiddleware(allEpics)
