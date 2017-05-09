d689ec857c9bbb26eb4cd650f0b65b89633cd22b
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-09 11:29:03 +0500

    Create and Implement Product Epic.js

---
+++ src/store/epics/productEpic.js
@@ -0,0 +1,50 @@
+import * as firebase from "firebase";
+import { Observable } from 'rxjs'
+// import { BranchAndOtherActions } from '../actions'
+
+export class ProductEpic {
+
+    // Create Product with user uid on firebase database
+    static createProduct = (action$) =>
+        action$.ofType('SET_PRODUCT')
+            .switchMap(({ payload }) => {
+                return firebase.database().ref('/').child(`product/${ProductEpic.getLocalStorage().uid}`).push(payload)
+                    .then((response) => {
+                        return {
+                            type: 'SET_PRODUCT_SUCCESS'
+                        }
+                    })
+                    .catch((error) => {
+                        return Observable.of({
+                            type: 'SET_PRODUCT_FAIL',
+                            payload: error.code
+                        })
+                    })
+            })
+
+    // Get all Products on firebase database through user uid 
+    // static getProduct = (action$) => {
+    //     return action$.ofType('LOGIN_SUCCESS')
+    //         .switchMap(({ payload }) => {
+    //             if (payload) {
+    //                 firebase.database().ref('/').child(`customer/${payload.uid}`).on("value", (snapshot) => {
+    //                     if (snapshot.val()) {
+    //                         BranchAndOtherActions.getAllCustomers(snapshot.val())
+    //                     }
+    //                 })
+    //             }
+    //             return Observable.of({
+    //                 type: 'GET_CUSTOMER_FAIL',
+    //                 // payload: {}
+    //             })
+
+    //         })
+
+    // }
+
+
+    static getLocalStorage() {
+        return JSON.parse(localStorage.getItem('store'));
+    }
+}
+
