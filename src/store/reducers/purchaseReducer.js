93360ca4b5aaf9da1362a515edbf476fc5b3ca86
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-10 18:38:35 +0500

    Create Purchase Reducer and implement logic

---
+++ src/store/reducers/purchaseReducer.js
@@ -0,0 +1,41 @@
+// import { BranchAndOtherActions } from '../actions'
+const initialState = {
+    isLoading: false,
+    isResult: false,
+    // branches: {},
+    // productsDetail: {},
+    isError: false,
+    errorMSG: {},
+    // data: [],
+    // twoKeyList: []
+}
+export const purchaseReducer = (state = initialState, action) => {
+    let newState = state
+    switch (action.type) {
+        // Action handlers
+        case 'SET_RESULT_FALSE':
+            return Object.assign({}, state, { isResult: false })
+
+        case 'SET_PURCHASE':
+            return Object.assign({}, state, { isLoading: true })
+
+        case 'SET_PURCHASE_SUCCESS':
+            newState['isLoading'] = false;
+            newState['isResult'] = true
+            return Object.assign({}, state, newState)
+
+        case 'SET_PURCHASE_FAIL':
+            newState['isError'] = true;
+            newState['isLoading'] = false
+            newState['errorMSG'] = action.payload
+            return Object.assign({}, state, newState)
+
+
+        // case 'GET_PURCHASE_SUCCESS':
+        //     newState['branches'] = Object.assign({}, action.payload);
+        //     return Object.assign({}, state, newState)
+
+        default:
+            return state;
+    }
+}
