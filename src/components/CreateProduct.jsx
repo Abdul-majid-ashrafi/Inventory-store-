4e0f3fb79d54913fbfb6be310261e29fb2d00233
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-22 06:06:40 +0500

    Update UI

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -18,15 +18,22 @@ export class CreateProductComponent extends Component {
         const center = {
             width: '60%',
             margin: '0 auto',
-            background: 'darkCyan',
+            background: '#fff',
             padding: '30px',
-            boxShadow: '0 15px 20px rgba(0, 0, 0, 0.3)'
+            boxShadow: '0 15px 20px rgba(0, 0, 0, 0.3)',
+            color: 'white'
+        }
+        const header = {
+            width: '65%',
+            margin: '0 auto',
+            boxShadow: '0 15px 20px rgba(0, 0, 0, 0.3)',
+            color: 'white'
         }
         return (
             <div style={{ marginTop: '30px' }}>
 
+                <mat.AppBar style={header} title="Create your product" showMenuIconButton={false} />
                 <div style={center}>
-                    <mat.AppBar title="Create your Product" showMenuIconButton={false} />
                     <form onSubmit={this.props._submit}>
                         <mat.TextField
                             hintText="Product Name"
