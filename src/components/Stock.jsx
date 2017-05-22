5433b2ae9f44fb6eb786f6d0225bda039c926f62
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-22 06:06:14 +0500

    Update UI

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -23,55 +23,51 @@ export class StockComponent extends Component {
 
     render() {
         const center = {
-            width: '70%',
+            width: '98%',
             margin: '0 auto',
-            background: 'darkCyan',
+            background: '#3da3b1',//00bcd4
             boxShadow: '0 15px 20px rgba(0, 0, 0, 0.3)'
         }
         const container = {
-            textAlign: 'center',
-            width: '100%',
+            width: '90%',
             margin: '0 auto',
-            background: '#fff2e6',
-            boxShadow: '0 15px 20px rgba(0, 0, 0, 0.3)'
         }
-
         return (
-            <div style={container}>
-                <br />
-                <br />
-                <mat.RaisedButton label="Stock" primary={true} style={center} />
-                <mat.Table style={center}>
-                    <mat.TableBody displayRowCheckbox={false}>
-                        <mat.TableRow style={{ color: 'white', fontWeight: '600' }}>
-                            <mat.TableRowColumn>Item</mat.TableRowColumn>
-                            <mat.TableRowColumn>Discription</mat.TableRowColumn>
-                            <mat.TableRowColumn>Price</mat.TableRowColumn>
-                            <mat.TableRowColumn>Quantity</mat.TableRowColumn>
-                            <mat.TableRowColumn>Total Price</mat.TableRowColumn>
-                            {/*<mat.TableRowColumn>Department</mat.TableRowColumn>*/}
-                        </mat.TableRow>
+            <div style={{ marginTop: '30px' }}>
+                <div style={container}>
+                    <mat.AppBar title="Stock" showMenuIconButton={false} style={{ borderBottomLeftRadius: '1em', borderBottomRightRadius: '1em' }} />
+                    <mat.Table style={center}>
+                        <mat.TableBody displayRowCheckbox={false}>
+                            <mat.TableRow style={{ color: 'white', fontWeight: '600' }}>
+                                <mat.TableRowColumn>Item</mat.TableRowColumn>
+                                <mat.TableRowColumn>Discription</mat.TableRowColumn>
+                                <mat.TableRowColumn>Price</mat.TableRowColumn>
+                                <mat.TableRowColumn>Quantity</mat.TableRowColumn>
+                                <mat.TableRowColumn>Total Price</mat.TableRowColumn>
+                                {/*<mat.TableRowColumn>Department</mat.TableRowColumn>*/}
+                            </mat.TableRow>
 
-                        {this.state.stockArray.map((value, index) => {
-                            return (
-                                <mat.TableRow style={{ color: 'white' }} key={index}>
-                                    <mat.TableRowColumn>{value.productName}</mat.TableRowColumn>
-                                    <mat.TableRowColumn>{value.description}</mat.TableRowColumn>
-                                    <mat.TableRowColumn>{value.eachPrice}</mat.TableRowColumn>
-                                    <mat.TableRowColumn>{value.quantity}</mat.TableRowColumn>
-                                    <mat.TableRowColumn>{value.totalPrice}</mat.TableRowColumn>
-                                    {/*<mat.TableRowColumn>{value.department}</mat.TableRowColumn>*/}
-                                    {/*{(value.description) ?
+                            {this.state.stockArray.map((value, index) => {
+                                return (
+                                    <mat.TableRow style={{ color: 'white' }} key={index}>
+                                        <mat.TableRowColumn>{value.productName}</mat.TableRowColumn>
+                                        <mat.TableRowColumn>{value.description}</mat.TableRowColumn>
+                                        <mat.TableRowColumn>{value.eachPrice}</mat.TableRowColumn>
+                                        <mat.TableRowColumn>{value.quantity}</mat.TableRowColumn>
+                                        <mat.TableRowColumn>{value.totalPrice}</mat.TableRowColumn>
+                                        {/*<mat.TableRowColumn>{value.department}</mat.TableRowColumn>*/}
+                                        {/*{(value.description) ?
                                         <mat.TableRowColumn>{value.description}</mat.TableRowColumn> :
                                         'null'
                                     }*/}
-                                </mat.TableRow>
-                            )
-                        })}
-                    </mat.TableBody>
-                </mat.Table>
-                <br />
-                <br />
+                                    </mat.TableRow>
+                                )
+                            })}
+                        </mat.TableBody>
+                    </mat.Table>
+                    <br />
+                    <br />
+                </div>
             </div>
         )
     }
